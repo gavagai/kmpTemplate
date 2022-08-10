2d5440010e5ddda3b20680b92a68277016b0d9f8
don <don.robertson@me.com>
2022-08-10 13:52:28 -0700

    Add most dependencies.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     kotlin("android")
 }
 
-val composeVersion = "1.3.0" // DON
+val composeCompilerVersion = "1.3.0" // DON
 android {
     compileSdk = 33 // DON: was 32
     defaultConfig {
@@ -23,7 +23,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = composeVersion
+        kotlinCompilerExtensionVersion = composeCompilerVersion
     }
     kotlinOptions {
         jvmTarget = "1.8"
@@ -31,6 +31,11 @@ android {
     // DON: --
 }
 
+// DON: --
+val material3Version = "1.0.0-alpha15"
+val koinVersion = "3.2.0"
+val composeLibraryVersion = "1.2.0"
+// DON: --
 dependencies {
     implementation(project(":shared"))
     implementation("com.google.android.material:material:1.4.0")
@@ -38,7 +43,27 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.0")
 
     // DON: --
-    implementation("androidx.compose.runtime:runtime:1.2.0") // DON: upgrade to compose version soon
+    implementation("androidx.compose.runtime:runtime:1.2.0") // DON: Compose compiler dependency (upgrade soon)
 
+    // Jetpack Compose
+    implementation("androidx.activity:activity-compose:1.5.1")
+    implementation("androidx.compose.ui:ui:$composeLibraryVersion")
+
+    // Material Design
+    implementation("androidx.compose.material3:material3:$material3Version")
+    implementation("androidx.compose.material3:material3-window-size-class:$material3Version")
+    implementation("androidx.compose.material:material-icons-core:$composeLibraryVersion")
+    implementation("androidx.compose.material:material-icons-extended:$composeLibraryVersion")
+    // Tooling support (Previews, etc.)
+    implementation("androidx.compose.ui:ui-tooling:$composeLibraryVersion")
+    // Foundation (Border, Background, Box, Image, Scroll, shapes, animations, etc.)
+    implementation("androidx.compose.foundation:foundation:$composeLibraryVersion")
+
+    // Koin core - https://insert-koin.io/docs/reference/koin-mp/kmp
+    implementation("io.insert-koin:koin-core:$koinVersion")
+    // Koin main features for Android - https://insert-koin.io/docs/reference/koin-android/start
+    implementation("io.insert-koin:koin-android:$koinVersion")
+    // Koin for Jetpack Compose - https://insert-koin.io/docs/reference/koin-android/compose
+    implementation("io.insert-koin:koin-androidx-compose:$koinVersion")
     // DON: --
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -16,14 +16,51 @@ kotlin {
         }
     }
 
+// DON: --
+    val ktorVersion = "2.0.2"
+    val koinVersion = "3.2.0"
+    val mviKotlinVersion = "3.0.0-beta01"
+// DON: --
     sourceSets {
-        val commonMain by getting
+        val commonMain by getting {
+            // DON: --
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
+
+                // Kotlin datetime
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.3")
+
+                // Ktor client
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+
+                // Koin core - https://insert-koin.io/docs/reference/koin-mp/kmp
+                implementation("io.insert-koin:koin-core:$koinVersion")
+
+                // MVIKotlin - https://github.com/arkivanov/MVIKotlin#readme
+                implementation("com.arkivanov.mvikotlin:mvikotlin:$mviKotlinVersion")
+                implementation("com.arkivanov.mvikotlin:mvikotlin-main:$mviKotlinVersion")
+                implementation("com.arkivanov.mvikotlin:mvikotlin-extensions-coroutines:$mviKotlinVersion")
+                implementation("com.arkivanov.mvikotlin:mvikotlin-logging:$mviKotlinVersion")
+            }
+            // DON: --
+        }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
             }
         }
-        val androidMain by getting
+        val androidMain by getting {
+            // DON: --
+            dependsOn(commonMain)
+            dependencies {
+                implementation("io.ktor:ktor-client-android:$ktorVersion")
+                implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+                implementation("androidx.compose.runtime:runtime:1.2.0") // Compose compiler dependency
+            }
+            // DON: --
+        }
         val androidTest by getting
         val iosX64Main by getting
         val iosArm64Main by getting
@@ -33,6 +70,11 @@ kotlin {
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
+            // DON: --
+            dependencies {
+                implementation("io.ktor:ktor-client-darwin:$ktorVersion")
+            }
+            // DON: --
         }
         val iosX64Test by getting
         val iosArm64Test by getting
