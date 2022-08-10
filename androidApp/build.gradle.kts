0bb0ebcdb1582d67eee1a1254629b1d88e62064e
don <don.robertson@me.com>
2022-08-10 13:33:12 -0700

    Upgrade to Compose 1.3.0 (mostly)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -3,8 +3,9 @@ plugins {
     kotlin("android")
 }
 
+val composeVersion = "1.3.0" // DON
 android {
-    compileSdk = 32
+    compileSdk = 33 // DON: was 32
     defaultConfig {
         applicationId = "com.teddyfreddy.kmp.android"
         minSdk = 21
@@ -17,6 +18,17 @@ android {
             isMinifyEnabled = false
         }
     }
+    // DON: --
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = composeVersion
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    // DON: --
 }
 
 dependencies {
@@ -24,4 +36,9 @@ dependencies {
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+
+    // DON: --
+    implementation("androidx.compose.runtime:runtime:1.2.0") // DON: upgrade to compose version soon
+
+    // DON: --
 }
