d506fd45cd8d7e9b5a580e5ed40c2dbf94510cc1
don <don.robertson@me.com>
2022-08-10 12:52:00 -0700

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/teddyfreddy/kmp/Greeting.kt
@@ -0,0 +1,7 @@
+package com.teddyfreddy.kmp
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/teddyfreddy/kmp/Platform.kt
@@ -0,0 +1,5 @@
+package com.teddyfreddy.kmp
+
+expect class Platform() {
+    val platform: String
+}
